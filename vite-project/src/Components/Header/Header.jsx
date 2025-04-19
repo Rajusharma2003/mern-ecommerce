920e3748cfa5fc0daed165bc3ababae91250217b
raju sharma <rajusharma50154@gmail.com>
2025-04-19 22:35:54 +0530

    90% work is complete on navbar

--- vite-project/src/Components/Header/Header.jsx
+++ vite-project/src/Components/Header/Header.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Search from '../Search'
 
+// material ui cart
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import { IoCartSharp } from "react-icons/io5";
+
+// cart
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    right: -3,
+    top: 13,
+    border: `2px solid ${(theme.vars ?? theme).palette.background.paper}`,
+    padding: '0 4px',
+  },
+}));
 
 
 const Header = () => {
@@ -30,15 +46,12 @@ const Header = () => {
             </ul>
           </div>
           </div>
-
-         
-         
       </div>
     </div>
 
 
      {/* This is the navber section */}
-     <div className="header">
+     <div className="header pt-2">
             <div className="container flex items-center justify-between">
 
               <div className="col1 w-[25%]">
@@ -46,12 +59,27 @@ const Header = () => {
                     <img src="https://serviceapi.spicezgold.com/download/1744255975457_logo.jpg" alt="" />
                   </Link>
               </div>
-
+            
+              {/* search button section */}
               <div className="col2 w-[45%]">
-                
+                <Search/>
               </div>
 
-              <div className="col1 w-[30%]">
+              <div className="col1 w-[30%] flex items-center pl-7">
+                <ul className="flex items-center gap-3">
+                  <li className="list-none">
+                    <Link to="/login" className='link transition text-[15px] font-[500]'>Login</Link> / &nbsp;
+                    <Link to="register" className='link transition text-[15px] font-[500]'>Register</Link>
+                  </li>
+
+                  <li>
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <IoCartSharp />
+                    </StyledBadge>
+                  </IconButton>
+                  </li>
+                </ul>
                 
               </div>
             </div>
--- vite-project/src/Components/Search/index.jsx
+++ vite-project/src/Components/Search/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import "../Search/Style.css"
+// import material ui
+import Button from '@mui/material/Button';
+// import react icon
+import { IoSearch } from "react-icons/io5";
 
 const Search = () => {
   return (
     <div className='searchBox w-[100%] h-[50%] bg-[#e5e5e5] rounded-[5px] relative p-2'>
-      <input type="text" placeholder='Search for Products...' className='w-full h-[35px] focus:outline-none bg-inherit p-2 text-[15px]' />
-      <button></button>
+      <input type="text" placeholder='Search for Products...' className='w-full h-[30px] focus:outline-none bg-inherit p-2 text-[15px]' />
+      <Button className='!absolute top-[5px] right-[5px] w-[34px] !min-w-[35px] h-[35px] !rounded-full z-50 !text-black'>
+        <IoSearch className='text-black'/>
+        </Button>
     </div>
   )
 }
